3dcb7a7ab594a0aa284ade6d78191527d3123008
PieroHua <piale820@gmail.com>
2025-04-12 10:36:04 -0500

    trabajo1

---
+++ Login.php
@@ -0,0 +1,11 @@
+<div class="login-wrap">
+  <h2>Login</h2>
+  <div class="form">
+    <input type="text" placeholder="Username" name="un" />
+    <input type="password" placeholder="Password" name="pw" />
+    <link rel="stylesheet" href="/Semana3/Login.css">
+    <script source="Semana3/Login.js"></script>
+    <button> Sign in </button>
+    <a href="#"> <p> Don't have an account? Register </p></a>
+  </div>
+</div>
